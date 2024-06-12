40163e87c65bc3c74d1386323ee6373b96332bea
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2024-06-12 14:55:12 +0530

    Update vendor_profile_completion_screen.dart

--- lib/vendor_profile_completion_screen.dart
+++ lib/vendor_profile_completion_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'auth_provider.dart';
 import 'map_screen.dart';
+import 'dart:io' show File; // Import for mobile platforms
+import 'package:flutter/foundation.dart'
+    show kIsWeb; // Check if platform is web
+import 'dart:typed_data'; // For web files
 
 class VendorProfileCompletionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Complete Profile'),
@@ -30,15 +31,15 @@ class VendorProfileForm extends StatefulWidget {
 class _VendorProfileFormState extends State<VendorProfileForm> {
   final _formKey = GlobalKey<FormState>();
   String? _gstin, _businessName;
-  List<File> _businessPhotos = [];
-  File? _panCard;
+  List<XFile> _businessPhotos = [];
+  XFile? _panCard;
   final ImagePicker _picker = ImagePicker();
 
   Future<void> _pickBusinessPhotos() async {
     final pickedFiles = await _picker.pickMultiImage();
     if (pickedFiles != null) {
       setState(() {
-        _businessPhotos = pickedFiles.map((file) => File(file.path)).toList();
+        _businessPhotos = pickedFiles;
       });
     }
   }
@@ -47,73 +48,107 @@ class _VendorProfileFormState extends State<VendorProfileForm> {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
-        _panCard = File(pickedFile.path);
+        _panCard = pickedFile;
       });
+    }
+  }
+
+  Future<void> _completeProfile(BuildContext context) async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      if (_panCard != null && _businessPhotos.isNotEmpty) {
+        try {
+          if (kIsWeb) {
+            // Handling for web
+            List<Uint8List> businessPhotosData = [];
+            for (var xFile in _businessPhotos) {
+              var bytes = await xFile.readAsBytes();
+              businessPhotosData.add(bytes);
+            }
+
+            Uint8List panCardData = await _panCard!.readAsBytes();
+
+            await authProvider.saveVendorProfileWeb(
+              _gstin ?? '',
+              _businessName!,
+              businessPhotosData,
+              panCardData,
+              _businessPhotos.map((xFile) => xFile.name).toList(),
+              _panCard!.name,
+            );
+          } else {
+            // Handling for mobile (or other platforms)
+            List<File> businessPhotosFiles =
+                _businessPhotos.map((xFile) => File(xFile.path)).toList();
+            File panCardFile = File(_panCard!.path);
+
+            await authProvider.saveVendorProfile(
+              _gstin ?? '',
+              _businessName!,
+              businessPhotosFiles,
+              panCardFile,
+            );
+          }
+
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => MapScreen()),
+          );
+        } catch (error) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Failed to save profile: $error'),
+          ));
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Please upload business photos and PAN card.'),
+        ));
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final authProvider = Provider.of<AuthProvider>(context);
-
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(labelText: 'GSTIN (Optional)'),
-            onSaved: (value) => _gstin = value,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Business Name'),
-            validator: (value) =>
-                value!.isEmpty ? 'Enter a business name' : null,
-            onSaved: (value) => _businessName = value,
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: _pickBusinessPhotos,
-            child: Text('Upload Business Photos'),
-          ),
-          _businessPhotos.isNotEmpty
-              ? Text('${_businessPhotos.length} photos selected')
-              : Container(),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: _pickPanCard,
-            child: Text('Upload PAN Card'),
-          ),
-          _panCard != null ? Text('PAN Card selected') : Container(),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              if (_formKey.currentState!.validate()) {
-                _formKey.currentState!.save();
-                if (_panCard != null && _businessPhotos.isNotEmpty) {
-                  authProvider
-                      .saveVendorProfile(_gstin ?? '', _businessName!,
-                          _businessPhotos, _panCard!)
-                      .then((_) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => MapScreen()),
-                    );
-                  }).catchError((error) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text('Failed to save profile: $error'),
-                    ));
-                  });
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content:
-                        Text('Please upload business photos and PAN card.'),
-                  ));
-                }
-              }
-            },
-            child: Text('Complete Profile'),
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: 'GSTIN (Optional)'),
+              onSaved: (value) => _gstin = value,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Business Name'),
+              validator: (value) =>
+                  value!.isEmpty ? 'Enter a business name' : null,
+              onSaved: (value) => _businessName = value,
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _pickBusinessPhotos,
+              child: Text('Upload Business Photos'),
+            ),
+            _businessPhotos.isNotEmpty
+                ? Text('${_businessPhotos.length} photos selected')
+                : Container(),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _pickPanCard,
+              child: Text('Upload PAN Card'),
+            ),
+            _panCard != null ? Text('PAN Card selected') : Container(),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _completeProfile(context),
+              child: Text('Complete Profile'),
+            ),
+          ],
+        ),
       ),
     );
   }
